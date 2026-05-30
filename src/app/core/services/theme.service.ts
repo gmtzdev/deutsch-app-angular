24ba035b865d50a0747dbce88770990a215fe618
gmtzdev <gerardomtzisc@gmail.com>
2026-05-30 12:40:26 -0600

    ✨ feat: refactor theme initialization logic to prioritize saved theme over system preference

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -8,15 +8,25 @@ export class ThemeService {
   readonly icon = computed(() => this.dark() ? 'pi-moon' : 'pi-sun');
 
   initTheme(): void {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    this.dark.set(prefersDark);
-    if (prefersDark) {
-      document.querySelector('html')!.classList.add('dark');
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      this.dark.set(savedTheme === 'dark');
+      if (savedTheme === 'dark') {
+        document.querySelector('html')!.classList.add('dark');
+      }
+    } else {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      this.dark.set(prefersDark);
+      if (prefersDark) {
+        document.querySelector('html')!.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      }
     }
   }
 
   toogleTheme(): void {
     document.querySelector('html')!.classList.toggle('dark');
     this.dark.update(v => !v);
+    localStorage.setItem('theme', this.dark() ? 'dark' : 'light');
   }
 }
